9d0bef601b3bfb25b19af938a0b17088fde00d79
U-cxue-PC\cxue <cxue@cxue-PC.(none)>
2014-06-08 22:17:48 +0800

    add dir and add create dialog

--- source/Global/globaldef.h
+++
@@ -1,8 +0,0 @@
-#ifndef GLOBALDEF_H
-#define GLOBALDEF_H
-
-static const char* BlockImageBasePath = ":/images/PopUps";
-static const char* BlockConfigPath = ":/config/config.json";
-static const char* ConstraintsBasePath = ":/images/Constraints";
-
-#endif // GLOBALDEF_H
